5722785558b295daa80dd9cc6bba6b413ea491d4
sui-tyan <mallari23adrian@gmail.com>
2025-12-19 21:09:44 +0800

    refactor: divided input validation to 2 input checks
    divided checking for isArray and array length to two input validation

--- api/app/controllers/form_schema_controller.js
+++ api/app/controllers/form_schema_controller.js
@@ -5,14 +5,30 @@ const addFormSchema = async (req, res) => {
   try {
     const formData = req.body;
 
-    if (!Array.isArray(formData) || formData.length === 0) {
-      return res.status(400).json({ message: "Invalid form schema data" });
+    if (!Array.isArray(formData)) {
+      return res.status(400).json({
+        statusCode: 400,
+        message: "FormData must be an array",
+        data: null,
+      });
+    }
+
+    if (formData.length === 0) {
+      return res.status(400).json({
+        statusCode: 400,
+        message: "At least one field is required",
+        data: null,
+      });
     }
 
     const newFormSchema = new FormSchema({ fields: formData });
     const savedSchema = await newFormSchema.save();
 
-    res.status(201).json({ schemaId: savedSchema._id });
+    res.status(201).json({
+      statusCode: 201,
+      message: "Form schema created successfully",
+      schemaId: savedSchema._id,
+    });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
   }
@@ -42,7 +58,6 @@ const deleteFormSchema = async (req, res) => {
 
     const result = await FormSchema.findByIdAndDelete(id);
 
-    console.log(result);
     if (!result) {
       return res.status(404).json({ message: "Form schema not found" });
     }
